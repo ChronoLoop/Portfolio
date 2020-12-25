b556aa59a32b6e1b90eb3aee2e3c19af76ec900f
ikevinws <kevinwang1036@gmail.com>
2020-12-25 08:19:46 -0500

    update project cards

--- src/components/projectCard/projectCard.js
+++ src/components/projectCard/projectCard.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import './projectCard.scss';
-import { Col, Card, Button } from 'react-bootstrap';
+import { Col, Card, Button, Image } from 'react-bootstrap';
 import { FaCode, FaArrowRight } from 'react-icons/fa';
 
 const ProjectCard = ({ projectInfo }) => {
     return (
-        <Col sm={6} className="p-0 my-2">
-            <Card className="project-card" bg="light">
+        <Col md={12} xl={6} className="p-0 my-2">
+            <Card className="project-card h-100 justify-content-between" bg="light">
                 <Card.Header as="h4">{projectInfo.name}</Card.Header>
-                <Card.Body>
-                    <Card.Text>{projectInfo.information}</Card.Text>
+                <div className="card-content">
+                    <p>{projectInfo.information}</p>
                     <div className="card-btns-container">
-                        <Button
-                            variant="light"
-                            href={projectInfo.visit}
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            className="card-btn"
-                        >
-                            <FaArrowRight className="mr-1" />
-                            Visit
-                        </Button>
                         <Button
                             variant="light"
                             href={projectInfo.github}
@@ -31,9 +21,27 @@ const ProjectCard = ({ projectInfo }) => {
                             <FaCode className="mr-1" />
                             Code
                         </Button>
+                        {projectInfo.visit ? (
+                            <Button
+                                variant="light"
+                                href={projectInfo.visit}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                className="card-btn"
+                            >
+                                <FaArrowRight className="mr-1" />
+                                Visit
+                            </Button>
+                        ) : null}
                     </div>
-                </Card.Body>
-                <Card.Footer className="text-center">hello</Card.Footer>
+                </div>
+                <Card.Footer>
+                    <div className="d-flex flex-wrap justify-content-center">
+                        {projectInfo.icons.map((icon) => {
+                            return <Image src={icon} alt="icon" className="card-icon mr-3" />;
+                        })}
+                    </div>
+                </Card.Footer>
             </Card>
         </Col>
     );
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { CardDeck } from 'react-bootstrap';
 import './projects.scss';
 import ProjectCard from '../projectCard/projectCard';
+// icons
+import webIcons from '../../assets/img/skills/html-css-js.svg';
+import reactIcon from '../../assets/img/skills/react.svg';
+import nodejsIcon from '../../assets/img/skills/nodejs.svg';
+import express from '../../assets/img/skills/express.svg';
+import mongodbIcon from '../../assets/img/skills/mongodb.svg';
+import herokuIcon from '../../assets/img/skills/heroku.svg';
+import unityIcon from '../../assets/img/skills/unity.svg';
+import csharpIcon from '../../assets/img/skills/c-sharp.svg';
 
 const Projects = [
     {
@@ -10,7 +19,23 @@ const Projects = [
             'A full-stack (MERN) web app for the Tower of Hanoi game where players can create accounts, which will allow scores to be posted onto a leaderboard. The game was built in Unity3D and exported as a WebGL.',
         github: 'https://github.com/ikevinws/TowerOfHanoi_WebApp',
         visit: 'https://tower-of-hanoi-kevin.herokuapp.com',
-        icons: []
+        icons: [
+            unityIcon,
+            csharpIcon,
+            reactIcon,
+            nodejsIcon,
+            express,
+            mongodbIcon,
+            herokuIcon,
+            webIcons
+        ]
+    },
+    {
+        name: 'MyMovieList',
+        information:
+            'MyMovieList is a simple movie database web app with plot summaries, ratings, and reviews. (In Progress)',
+        github: 'https://github.com/ikevinws/MyMovieList',
+        icons: [reactIcon, nodejsIcon, express, mongodbIcon, webIcons]
     }
 ];
 const ProjectsComponent = () => {
