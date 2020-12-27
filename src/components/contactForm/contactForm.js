30ad5631493f5caaf16f48ec868d1d3d7d655cd2
ikevinws <kevinwang1036@gmail.com>
2020-12-27 09:27:52 -0500

    cleanup contact section

---
+++ src/components/contactForm/contactForm.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import emailjs from 'emailjs-com';
+
+const ContactForm = ({ emailErrorHandler, emailSuccessHandler, validFormHandler }) => {
+    const [validated, setValidated] = useState(false);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const resetForm = () => {
+        setFormData({
+            name: '',
+            email: '',
+            message: ''
+        });
+    };
+
+    const sendEmail = async (data) => {
+        try {
+            await emailjs.send(
+                'service_r9u5wuc',
+                'template_vkvcwvc',
+                data,
+                'user_y7mncT0M4K0nUfhB2xhoc'
+            );
+            emailSuccessHandler();
+        } catch (err) {
+            emailErrorHandler();
+        }
+    };
+
+    const handleSubmit = (e) => {
+        const form = e.currentTarget;
+        e.preventDefault();
+
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+            setValidated(true);
+        } else {
+            validFormHandler();
+            sendEmail({ ...formData });
+            setValidated(false);
+            resetForm();
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    return (
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <div>
+                <Form.Label className="m-0 mt-1">Name:</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter Your Name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div>
+                <Form.Label className="m-0 mt-1">Email:</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Enter Your Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div>
+                <Form.Label className="m-0 mt-1">Message:</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={4}
+                    placeholder="Write Your Message "
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <Button type="submit" className="form-btn mt-2">
+                Send Message
+            </Button>
+        </Form>
+    );
+};
+
+export default ContactForm;
--- src/sections/contact/contact.js
+++ src/sections/contact/contact.js
@@ -1,60 +1,22 @@
 import React, { useState } from 'react';
 import './contact.scss';
-import { Col, Form, Container, Button, Alert } from 'react-bootstrap';
+import { Col, Container, Alert } from 'react-bootstrap';
 import { FaEnvelope, FaLinkedinIn } from 'react-icons/fa';
-import emailjs from 'emailjs-com';
+import ContactForm from '../../components/contactForm/contactForm';
 
 const Contact = () => {
     const [showError, setShowError] = useState(false);
     const [showSuccess, setShowSuccess] = useState(false);
-    const [validated, setValidated] = useState(false);
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
 
-    const resetForm = () => {
-        setFormData({
-            name: '',
-            email: '',
-            message: ''
-        });
+    const emailErrorHandler = () => {
+        setShowError(true);
     };
-
-    const sendEmail = async (data) => {
-        try {
-            await emailjs.send(
-                'service_r9u5wuc',
-                'template_vkvcwvc',
-                data,
-                'user_y7mncT0M4K0nUfhB2xhoc'
-            );
-            setShowSuccess(true);
-        } catch (err) {
-            setShowError(!showError);
-        }
+    const emailSuccessHandler = () => {
+        setShowSuccess(true);
     };
-
-    const handleSubmit = (e) => {
-        const form = e.currentTarget;
-        e.preventDefault();
-
-        if (form.checkValidity() === false) {
-            e.stopPropagation();
-            setValidated(true);
-        } else {
-            setShowError(false);
-            setShowSuccess(false);
-            sendEmail({ ...formData });
-            setValidated(false);
-            resetForm();
-        }
-    };
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+    const validFormHandler = () => {
+        setShowError(false);
+        setShowSuccess(false);
     };
 
     return (
@@ -77,82 +39,46 @@ const Contact = () => {
 
                 <div className="contact-content">
                     <Col md={7} className="p-0 pr-md-3">
-                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                            <div>
-                                <Form.Label className="m-0 mt-1">Name:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter Your Name"
-                                    name="name"
-                                    value={formData.name}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <Form.Label className="m-0 mt-1">Email:</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Enter Your Email"
-                                    name="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <Form.Label className="m-0 mt-1">Message:</Form.Label>
-                                <Form.Control
-                                    as="textarea"
-                                    rows={4}
-                                    placeholder="Write Your Message "
-                                    name="message"
-                                    value={formData.message}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <Button type="submit" className="form-btn mt-2">
-                                Send Message
-                            </Button>
-                        </Form>
+                        <ContactForm
+                            emailErrorHandler={emailErrorHandler}
+                            emailSuccessHandler={emailSuccessHandler}
+                            validFormHandler={validFormHandler}
+                        />
                     </Col>
                     <Col
                         md={5}
-                        className="d-flex flex-column justify-content-md-center align-items-md-center p-0 ml-auto mt-3 mt-md-0"
+                        className="d-flex flex-column justify-content-md-center align-items-md-center p-0 mt-3 mt-md-0"
                     >
-                        <div className="d-block">
-                            <div className="mb-3">
-                                <div className="contact-container">
-                                    <FaEnvelope className="mr-1" />
-                                    Email
-                                </div>
-                                <div>
-                                    <a
-                                        href="mailto:kevinwang1036@gmail.com"
-                                        rel="noopener noreferrer"
-                                        target="_blank"
-                                        className="contact-link"
-                                    >
-                                        kevinwang1036@gmail.com
-                                    </a>
-                                </div>
-                            </div>
+                        <div className="contact-container mb-3">
+                            <h5>
+                                <FaEnvelope className="mr-1" />
+                                Email
+                            </h5>
                             <div>
-                                <div className="contact-container">
-                                    <FaLinkedinIn className="mr-1" />
-                                    Linkedin
-                                </div>
-                                <div>
-                                    <a
-                                        href="https://linkedin.com/in/ikevinwang"
-                                        rel="noopener noreferrer"
-                                        target="_blank"
-                                        className="contact-link"
-                                    >
-                                        linkedin.com/in/ikevinwang
-                                    </a>
-                                </div>
+                                <a
+                                    href="mailto:kevinwang1036@gmail.com"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="contact-link"
+                                >
+                                    kevinwang1036@gmail.com
+                                </a>
+                            </div>
+                        </div>
+                        <div className="contact-container">
+                            <h5>
+                                <FaLinkedinIn className="mr-1" />
+                                Linkedin
+                            </h5>
+                            <div>
+                                <a
+                                    href="https://linkedin.com/in/ikevinwang"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="contact-link"
+                                >
+                                    linkedin.com/in/ikevinwang
+                                </a>
                             </div>
                         </div>
                     </Col>
